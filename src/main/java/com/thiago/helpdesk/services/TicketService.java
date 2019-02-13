5f7ae1a1bae193523274bc892f40d876bab7a203
Thiago Amaral <thiago-amaral@msn.com>
2019-02-13 14:27:01 -0200

    Criando o Serviço de Ticket

---
+++ src/main/java/com/thiago/helpdesk/services/TicketService.java
@@ -0,0 +1,36 @@
+package com.thiago.helpdesk.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import com.thiago.helpdesk.entity.ChangeStatus;
+import com.thiago.helpdesk.entity.Ticket;
+
+@Component
+public interface TicketService {
+
+	Ticket createOrUpdate(Ticket ticket);
+	
+	Ticket findById(String id);
+	
+	void delete(String id);
+	
+	Page<Ticket> listTicket(int page, int count);
+	
+	ChangeStatus createChangeStatus(ChangeStatus changeStatus);
+	
+	Iterable<ChangeStatus> listChangeStatus(String ticketId);
+	
+	Page<Ticket> findByCurrentUser(int page, int count, String userId);
+	
+	Page<Ticket> findByParameters(int page, int count,String title,String status,String priority);
+	
+	Page<Ticket> findByParametersAndCurrentUser(int page, int count,String title,String status,String priority,String userId);
+	
+	Page<Ticket> findByNumber(int page, int count,Integer number);
+	
+	Iterable<Ticket> findAll();
+	
+	public Page<Ticket> findByParametersAndAssignedUser(int page, int count,String title,String status,String priority,String assignedUserId);
+
+}
---
+++ src/main/java/com/thiago/helpdesk/services/impl/TicketServiceImpl.java
@@ -0,0 +1,86 @@
+package com.thiago.helpdesk.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import com.thiago.helpdesk.entity.ChangeStatus;
+import com.thiago.helpdesk.entity.Ticket;
+import com.thiago.helpdesk.repository.ChangeStatusRepository;
+import com.thiago.helpdesk.repository.TicketRepository;
+import com.thiago.helpdesk.services.TicketService;
+
+@Component
+public class TicketServiceImpl implements TicketService {
+
+	@Autowired
+	private TicketRepository ticketRepository;
+
+	@Autowired
+	private ChangeStatusRepository changeStatusRepository;
+
+	public Ticket createOrUpdate(Ticket ticket) {
+		return this.ticketRepository.save(ticket);
+	}
+
+	public Ticket findById(String id) {
+		return this.ticketRepository.findOne(id);
+	}
+
+	public void delete(String id) {
+		this.ticketRepository.delete(id);
+	}
+
+	public Page<Ticket> listTicket(int page, int count) {
+		Pageable pages = new PageRequest(page, count);
+		return this.ticketRepository.findAll(pages);
+	}
+
+	public Iterable<Ticket> findAll() {
+		return this.ticketRepository.findAll();
+	}
+
+	public Page<Ticket> findByCurrentUser(int page, int count, String userId) {
+		Pageable pages = new PageRequest(page, count);
+		return this.ticketRepository.findByUserIdOrderByDateDesc(pages,userId);
+	}
+
+	public ChangeStatus createChangeStatus(ChangeStatus changeStatus) {
+		return this.changeStatusRepository.save(changeStatus);
+	}
+
+	public Iterable<ChangeStatus> listChangeStatus(String ticketId) {
+		return this.changeStatusRepository.findByTicketIdOrderByDateChangeStatusDesc(ticketId);
+	}
+
+	public Page<Ticket> findByParameters(int page, int count,String title,String status,String priority) {
+		Pageable pages = new PageRequest(page, count);
+		return this.ticketRepository.
+				findByTitleIgnoreCaseContainingAndStatusAndPriorityOrderByDateDesc(
+						title,status,priority,pages);
+	}
+
+	public Page<Ticket> findByParametersAndCurrentUser(int page, int count,String title,String status,
+			String priority,String userId) {
+		Pageable pages = new PageRequest(page, count);
+		return this.ticketRepository.
+				findByTitleIgnoreCaseContainingAndStatusAndPriorityAndUserIdOrderByDateDesc(
+						title,status,priority,userId,pages);
+	}
+
+	public Page<Ticket> findByNumber(int page, int count,Integer number){
+		Pageable pages = new PageRequest(page, count);
+		return this.ticketRepository.findByNumber(number, pages);
+	}
+
+	public Page<Ticket> findByParametersAndAssignedUser(int page, int count,String title,String status,
+			String priority,String assignedUserId) {
+		Pageable pages = new PageRequest(page, count);
+		return this.ticketRepository.
+				findByTitleIgnoreCaseContainingAndStatusAndPriorityAndAssignedUserIdOrderByDateDesc(
+						title,status,priority,assignedUserId,pages);
+	}
+
+}
