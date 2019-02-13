529868579d9a7dce56d002378427a6d6e5167714
Thiago Amaral <thiago-amaral@msn.com>
2019-02-13 14:04:06 -0200

    Autenticação e autorização com tokens JWT Parte 2

---
+++ src/main/java/com/thiago/helpdesk/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.thiago.helpdesk.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.thiago.helpdesk.entity.User;
+import com.thiago.helpdesk.security.jwt.JwtUserFactory;
+import com.thiago.helpdesk.services.UserService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userService.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("No user found with username '%s'.", email));
+		} else {
+			return JwtUserFactory.create(user);
+		}
+	}
+}
